4587f35e76788a904ff67029459804a869c3b994
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 13:17:31 -0500

    Created authentication component

---
+++ components/AuthComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthComponent = () => {
+  return (
+    <div>
+      {/* Authentication related components */}
+    </div>
+  );
+}
+
+export default AuthComponent;
