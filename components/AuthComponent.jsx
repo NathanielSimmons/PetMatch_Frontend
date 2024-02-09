2db811c21f20895588ebb941363f24af20343d74
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 13:42:17 -0500

    Updated authentication component

--- components/AuthComponent.jsx
+++ components/AuthComponent.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import LoginForm from './forms/LoginForm';
 
 const AuthComponent = () => {
   return (
     <div>
-      {/* Authentication related components */}
+      <h2>Login</h2>
+      <LoginForm />
     </div>
   );
 }
