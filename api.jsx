a30db498f4f12259ec74878b1c728c435682be1a
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 14:10:35 -0500

    Updated api.jsx file

--- api.jsx
+++ api.jsx
@@ -2,13 +2,68 @@ import axios from 'axios';
 
 const BASE_URL = 'http://localhost:4000/api';
 
-export const loginUser = async (userData) => {
-  try {
-    const response = await axios.post(`${BASE_URL}/login`, userData);
-    return response.data;
-  } catch (error) {
-    throw error.response.data;
-  }
-}
-
+// User API calls
+export const signupUser = async (userData) => {
+    try {
+      const response = await axios.post(`${baseURL}/users/signup`, userData);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.error);
+    }
+  };
+  
+  export const loginUser = async (userData) => {
+    try {
+      const response = await axios.post(`${baseURL}/users/login`, userData);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.error);
+    }
+  };
+  
+  // Pet API calls
+  export const createPetProfile = async (petData) => {
+    try {
+      const response = await axios.post(`${baseURL}/pets`, petData);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.error);
+    }
+  };
+  
+  export const getAllPets = async () => {
+    try {
+      const response = await axios.get(`${baseURL}/pets`);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.error);
+    }
+  };
+  
+  export const getPetById = async (petId) => {
+    try {
+      const response = await axios.get(`${baseURL}/pets/${petId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.error);
+    }
+  };
+  
+  export const updatePetProfile = async (petId, petData) => {
+    try {
+      const response = await axios.put(`${baseURL}/pets/${petId}`, petData);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.error);
+    }
+  };
+  
+  export const deletePetProfile = async (petId) => {
+    try {
+      const response = await axios.delete(`${baseURL}/pets/${petId}`);
+      return response.data;
+    } catch (error) {
+      throw new Error(error.response.data.error);
+    }
+  };
 // Implement other API call functions for user and pet data, etc.
