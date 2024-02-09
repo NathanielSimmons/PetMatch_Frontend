8705e3c8ac135d45860abb48e77c4fe1f8f4c3e8
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 13:51:14 -0500

    Updated app.jsx to handle the about page route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AuthComponent from './components/AuthComponent';
 import UserProfileComponent from './components/UserProfileComponent';
 import PetProfileComponent from './components/PetProfileComponent';
 import MatchManagementComponent from './components/MatchManagementComponent';
+import AboutComponent from './components/AboutComponent';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route exact path="/profile" component={UserProfileComponent} />
         <Route exact path="/pet-profile" component={PetProfileComponent} />
         <Route exact path="/match" component={MatchManagementComponent} />
+        <Route exact path="/about" component={AboutComponent} />
       </Switch>
     </Router>
   );
