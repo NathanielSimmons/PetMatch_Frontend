a1511355a26125036d7ea2fb02e3eb2952f6e8fb
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 13:24:59 -0500

    Created match management component

---
+++ components/MatchManagementComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MatchManagementComponent = () => {
+  return (
+    <div>
+      {/* Match management related components */}
+    </div>
+  );
+}
+
+export default MatchManagementComponent;
