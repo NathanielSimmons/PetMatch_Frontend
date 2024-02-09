808555ae7755c400dd663fae202a850c70ff57a0
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 13:23:24 -0500

    Created pet profile component

---
+++ components/PetProfileComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PetProfileComponent = () => {
+  return (
+    <div>
+      {/* Pet profile related components */}
+    </div>
+  );
+}
+
+export default PetProfileComponent;
