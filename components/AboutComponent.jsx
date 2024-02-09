8024419bbe0a1353eb7b5f2b91059683003e61c5
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 13:44:47 -0500

    Created an about component

---
+++ components/AboutComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutComponent = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our application! This is a brief overview of what our application does...</p>
+      {/* Add more content as needed */}
+    </div>
+  );
+}
+
+export default AboutComponent;
