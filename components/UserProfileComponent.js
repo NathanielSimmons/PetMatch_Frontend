4421e7d361def6b4c44dd4230d65a7326055120e
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-09 13:19:11 -0500

    created user profile component

---
+++ components/UserProfileComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UserProfileComponent = () => {
+  return (
+    <div>
+      {/* User profile related components */}
+    </div>
+  );
+}
+
+export default UserProfileComponent;
