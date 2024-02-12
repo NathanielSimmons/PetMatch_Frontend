ef1619501771c31480c571be1e658cf14c481661
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-12 14:28:53 -0500

    Updated app.jsx to handle login/logout status

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import { Routes, Route} from 'react-router-dom';
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import AuthComponent from '../components/AuthComponent';
 import UserProfileComponent from '../components/UserProfileComponent';
 import PetProfileComponent from '../components/PetProfileComponent';
@@ -7,22 +7,33 @@ import MatchManagementComponent from '../components/MatchManagementComponent';
 import AboutComponent from '../components/AboutComponent';
 import HomeComponent from '../components/HomeComponent';
 import NavBarComponent from '../components/NavBarComponent';
+import {logoutUser} from '../api'
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogout = async () => {
+    try {
+      await logoutUser(); // Assuming logoutUser function is implemented correctly
+      setIsLoggedIn(false);
+    } catch (error) {
+      console.error('Logout failed:', error);
+    }
+  };
 
   return (
     <div>
-    <NavBarComponent isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-    <Routes>
-        <Route path="/" element={<HomeComponent/>} />
-        <Route path="/auth" element={<AuthComponent/>} />
-        <Route path="/profile" element={<UserProfileComponent/>} />
-        <Route path="/pet-profile" element={<PetProfileComponent/>} />
-        <Route path="/match" element={<MatchManagementComponent/>} />
-        <Route path="/about" element= {<AboutComponent/>} />
-    </Routes>
+      <NavBarComponent isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+      <Routes>
+        <Route path="/" element={<HomeComponent />} />
+        <Route path="/auth" element={<AuthComponent />} />
+        <Route path="/profile" element={<UserProfileComponent />} />
+        <Route path="/pet-profile" element={<PetProfileComponent />} />
+        <Route path="/match" element={<MatchManagementComponent />} />
+        <Route path="/about" element={<AboutComponent />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
