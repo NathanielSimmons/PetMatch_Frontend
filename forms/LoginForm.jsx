2ce97748c03c6b85fa58d6cf4685a5dd6d261626
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-12 11:43:35 -0500

    Updated Login form

--- forms/LoginForm.jsx
+++ forms/LoginForm.jsx
@@ -2,26 +2,23 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../api';
 
-
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
-  const history = useNavigate();
+  const navigate = useNavigate(); // Using useNavigate instead of useHistory
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const userData = { email, password };
       const response = await loginUser(userData);
-      console.log('Login response:', response); // Log the response for debugging
       if (response.success) {
-        history.push('/about');
+        navigate('/home'); // Use navigate instead of history.push
       } else {
         setError('Unable to login. Please check your credentials.'); 
       }
     } catch (error) {
-      console.error('Login error:', error); // Log any errors for debugging
       setError('Internal server error. Please try again later.'); 
     }
   };
