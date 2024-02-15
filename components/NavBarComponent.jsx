9ed3c3f80354ea081aa37c3c260202beccfc3595
NathanielSimmons <nathaniel.cj.simmons@gmail.com>
2024-02-15 13:25:14 -0500

    Styled the match tab

--- components/NavBarComponent.jsx
+++ components/NavBarComponent.jsx
@@ -3,11 +3,18 @@ import { Link } from 'react-router-dom';
 import LogOutButtonComponent from './LogOutButtonComponent';
 import '../src/App.css'
 
-
 const NavBarComponent = ({ isLoggedIn, user }) => {
   return (
     <nav className="navbar">
       <ul className="nav-list">
+        <li className="user-info">
+          {isLoggedIn && user && (
+            <>
+              <img src={user.profilePic} alt="User" className="user-avatar" />
+              <span className="welcome-msg">Welcome {user.username}!</span>
+            </>
+          )}
+        </li>
         <li><Link to="/">Home</Link></li>
         {!isLoggedIn && (
           <>
@@ -19,7 +26,7 @@ const NavBarComponent = ({ isLoggedIn, user }) => {
         {isLoggedIn && (
           <>
             <li><Link to="/pet-list">Pets</Link></li>
-            <li><Link to={`/update-profile/${ user?._id }`}>Update Profile</Link></li>
+            <li><Link to={`/update-profile/${user?._id}`}>Update Profile</Link></li>
             <li><Link to="/match">Match</Link></li>
             <li><Link to="/about">About</Link></li>
             <li className="logout-btn"><LogOutButtonComponent /></li>
